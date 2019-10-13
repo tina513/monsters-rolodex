e055cb5d3b913fefbccfc7a3ed4a88f44a1d6883
daishanjiang3 <daishanjiang3@gatech.edu>
2019-10-13 19:08:48 -0400

    Updates

---
+++ precache-manifest.561f57d578c906dd43823bfe30594ac7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b3cec2a5e004dcb9f789e9b87769ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "76bf620e2586378e100f",
+    "url": "/monsters-rolodex/static/css/main.e724a076.chunk.css"
+  },
+  {
+    "revision": "71edca509bdc8304827a",
+    "url": "/monsters-rolodex/static/js/2.c0b4862f.chunk.js"
+  },
+  {
+    "revision": "76bf620e2586378e100f",
+    "url": "/monsters-rolodex/static/js/main.7ff5e365.chunk.js"
+  },
+  {
+    "revision": "90eb73f2824a91acb50d",
+    "url": "/monsters-rolodex/static/js/runtime-main.8d0c0b52.js"
+  }
+]);
